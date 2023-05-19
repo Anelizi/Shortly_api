d2ed64b19e68d97caa1978d8566deef9c03c489b
Anelizi <anelizicarvalho@gmail.com>
2023-05-19 20:57:03 -0300

    fixing: function login

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -6,7 +6,7 @@ export async function signUp(req, res) {
   const { name, email, password} = req.body;
 
   try {
-    const user = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+    const user = await db.query(`SELECT * FROM users WHERE email = $1;`, [email]);
 
     if (user.rowCount > 0){
      return res.status(409).send("E-mail já foi cadastrado!");
@@ -14,7 +14,7 @@ export async function signUp(req, res) {
 
     const hash = bcrypt.hashSync(password, 10);
 
-    await db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, hash]);
+    await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [name, email, hash]);
 
     res.sendStatus(201);
   } catch (error) {
@@ -26,7 +26,7 @@ export async function login(req, res) {
   const { email, password } = req.body;
 
   try {
-    const user = await db.query('SELECT id FROM users WHERE email = $1 AND password = $2', [email, password]);
+    const user = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
 
     if (user.rowCount === 0) {
     return res.status(401).send("Usuário/senha inválidos");
@@ -37,6 +37,8 @@ export async function login(req, res) {
     if (!isPasswordCorrect) return res.status(401).send("Usuário/senha inválidos");
 
     const token = uuid();
+
+    await db.query(`INSERT INTO sessions (token, "userId") VALUES ($1, $2);`, [token, user.rows[0].id]);
 
     res.send({ token });
   } catch (error) {
