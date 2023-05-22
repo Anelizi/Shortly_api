5eda41912d529b63460d9a8b5854816346265c8e
Anelizi <anelizicarvalho@gmail.com>
2023-05-21 21:05:13 -0300

    requests: GET /urls/:id and /urls/open/:shortUrl

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,21 +1,26 @@
 import { nanoid } from "nanoid";
-import { db } from "../database/database.connection";
+import { db } from "../database/database.connection.js";
 
 export async function postUrl(req, res) {
   const { url } = req.body;
   const { authorization } = req.headers;
-  const token = authorization?.replace("Bearer ", '');
+  const token = authorization?.replace("Bearer ", "");
   const shortUrl = nanoid();
 
   try {
-    const user = await db.query(`SELECT "userId" FROM sessions WHERE token = $1;`, [token]);
+    const user = await db.query(
+      `SELECT "userId" FROM sessions WHERE token = $1;`,
+      [token]
+    );
 
-    await db.query(`INSERT INTO urls (url, "shortUrl", "userId")
+    await db.query(
+      `INSERT INTO urls (url, "shortUrl", "userId")
       VALUES ($1, $2, $3);`,
       [url, shortUrl, user.rows[0].userId]
     );
 
-    const urls = await db.query(`SELECT id, "shortUrl"
+    const urls = await db.query(
+      `SELECT id, "shortUrl"
       FROM urls WHERE "shortUrl" = $1 AND "userId" = $2;`,
       [shortUrl, user.rows[0].userId]
     );
@@ -27,14 +32,30 @@ export async function postUrl(req, res) {
 }
 
 export async function getId(req, res) {
+  const { id } = req.params;
+
+  if (!parseInt(id)) return res.sendStatus(404);
+
   try {
+    const url = await db.query(`SELECT id, url, "shortUrl" 
+       FROM ulrs WHERE id =$1;`,
+      [id]
+    );
+
+    if (url.rowCount === 0) return res.sendStatus(404);
+
+    res.status(200).send(url.rows[0]);
   } catch (error) {
     res.status(500).send(error.message);
   }
 }
 
 export async function getOpen(req, res) {
+  const { shortUrl } = req.params;
   try {
+    const open = await db.query(`UPDATE urls SET "visitCount" = "visitCount" + 1 WHERE "shortUrl" = $1;`, [shortUrl]);
+
+    res.status(200).redirect(open.rows[0].url);
   } catch (error) {
     res.status(500).send(error.message);
   }
