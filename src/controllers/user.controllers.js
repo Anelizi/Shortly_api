52b0d1248aa7c14bf99a98e25fd0131459ee9368
Anelizi <anelizicarvalho@gmail.com>
2023-05-22 10:39:39 -0300

    requests: GET /users/me and /ranking

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,22 @@
+import { db } from "../database/database.connection.js";
+
 export async function getUser(req, res) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", '');
+  const user = await db.query(`SELECT "userId" FROM sessions WHERE token = $1;`, [token]);
+
   try {
+    const result = await db.query(`SELECT users.id, users.name,
+      SUM(urls."visitCount") AS "visitCount",
+      json_agg(JSON_BUILD_OBJECT('id', urls.id, 'shortUrl', urls."shortUrl", 'url', urls.url, 'visitCount', urls."visitCount")) AS "shortenedUrls"
+      FROM users
+      JOIN urls ON users.id = urls."userId"
+      WHERE users.id = $1
+      GROUP BY users.id;`,
+      [user.rows[0].userId]
+    );
+  
+    res.status(200).send(result.rows[0]);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -7,6 +24,17 @@ export async function getUser(req, res) {
 
 export async function getRanking(req, res) {
   try {
+    const ranking = await db.query(`SELECT users.id, users.name,
+      COUNT(urls.id) AS "linksCount",
+      SUM(urls."visitCount") AS "visitCount"
+      FROM users
+      LEFT JOIN urls ON users.id = urls."userId"
+      GROUP BY users.id
+      ORDER BY "visitCount" DESC
+      LIMIT 10;`
+    ); 
+
+    res.status(200).send(ranking.rows);
   } catch (error) {
     res.status(500).send(error.message);
   }
---
+++ src/repository/auth.repository.js
@@ -0,0 +1,9 @@
+// import { db } from "../database/database.connection.js";
+
+// export async function selectSingUp(email){
+//     return db.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+// }
+
+// export async function insertSingUp(name, email, hash){
+//     return db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [name, email, hash]);
+// }
